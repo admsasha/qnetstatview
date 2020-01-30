d14f29a29c9dc1cf3873796bdbaef3aa0ebe7eed
sasha <dik@inbox.ru>
2020-01-30 10:58:32 +0900

    version up

--- config_qnetstatview.h
+++ config_qnetstatview.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define QNETSTATVIEW_VERSION "1.5.5"
-#define QNETSTATVIEW_DATEBUILD "26.12.2019"
+#define QNETSTATVIEW_VERSION "1.5.6 dev"
+#define QNETSTATVIEW_DATEBUILD "xx.xx.xxxx"
 
 #endif // CONFIG_H
